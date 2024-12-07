942e4d0df070abb571e687d27076cd730100e9fc
khalender <khalenderin@hotmail.com>
2024-12-07 13:45:56 +0100

    initial code base

---
+++ backend/spotyjukeboxapp/src/main/java/org/bithub/SpotyJukeBoxApplication.java
@@ -0,0 +1,11 @@
+package org.bithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotyJukeBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpotyJukeBoxApplication.class, args);
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="flex flex-col w-full h-screen items-center justify-center">
+                <div className="text-center bg-red-500 text-white">
+                    Spoty Jukebox
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
