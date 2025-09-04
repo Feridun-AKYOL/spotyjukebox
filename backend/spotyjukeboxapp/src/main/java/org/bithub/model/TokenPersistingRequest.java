2bddf4c45246717c8fd6214d662243831d75c86d
feridun <feridunakyol34@gmail.com>
2025-09-04 18:05:27 +0200

    feat: no need to manual TokenPersistRequestValidator

--- backend/spotyjukeboxapp/src/main/java/org/bithub/model/TokenPersistingRequest.java
+++ backend/spotyjukeboxapp/src/main/java/org/bithub/model/TokenPersistingRequest.java
@@ -1,4 +1,8 @@
 package org.bithub.model;
 
-public record TokenPersistingRequest(String userId, String accessToken, String refreshToken) {
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Set;
+
+public record TokenPersistingRequest(@NotBlank String userId, @NotBlank String accessToken, @NotBlank String refreshToken, Set<String> scopes) {
 }
